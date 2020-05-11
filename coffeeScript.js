00dda6a6b09c91ebd6a5daddccc11a27921cade2
zan242 <zan242@users.noreply.github.com>
2020-05-11 19:59:06 -0400

    Almost Complete

--- coffeeScript.js
+++ coffeeScript.js
@@ -1,3 +1,15 @@
-var varietybutton = document.getElementById("vbutton");
+varietybutton = document.getElementById("vbutton");
 
-varietybutton.href = "coffeevarieties.html";
+varietybutton.addEventListener("click", onClick1);
+
+function onClick1() {  
+    window.location.href = "coffeevarieties.html"; 
+}
+
+brewbutton = document.getElementById("bbutton");
+
+brewbutton.addEventListener("click", onClick2);
+
+function onClick2() {  
+    window.location.href = "brewcoffee.html"; 
+}
---
+++ landingpageScript.js
@@ -0,0 +1,5 @@
+window.setTimeout(onClick, 5000);
+
+function onClick() {  
+    window.location.href = "homepage.html";
+}
